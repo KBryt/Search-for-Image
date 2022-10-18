170ff1557aebb4f813576b7bdb2b602ec4de8843
Bright Effah <99838742+KBryt@users.noreply.github.com>
2022-10-18 12:36:36 -0400

    Create Image2Image.sh

---
+++ Image2Image.sh
@@ -0,0 +1,5 @@
+ORM_META_SAMPLE_IMAGES="/content/drive/MyDrive/pics"
+
+python3 Image2Image.py \
+--input_image_product_metadata_filename="${ORM_META_SAMPLE_IMAGES}" \
+--input_request_image_filename='/content/drive/MyDrive/pics/1637.jpg' 
